fe156b4280039f02d3666efcfb15fe4c30115188
Claudio Altamura <claudio@claudioaltamura.de>
2019-06-17 09:21:35 +0200

    fix: getter

--- src/main/java/de/claudioaltamura/java/lombok/SuperHero.java
+++ src/main/java/de/claudioaltamura/java/lombok/SuperHero.java
@@ -2,11 +2,17 @@ package de.claudioaltamura.java.lombok;
 
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter 
+@Setter
 @EqualsAndHashCode
+@ToString
 public class SuperHero {
 
   private String name;
